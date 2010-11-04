876e059e79d8644df6c3ea324e73e304c2e58309
James Cook <mokus@deepbondi.net>
2010-11-04 15:41:20 +0000

    added lots of haddock comments and a couple more functions (constPoly and composePoly).  Also changed a few functions to error out on bad input, so I'm making a major-version bump instead of a minor.

--- src/Math/Polynomial.hs
+++ src/Math/Polynomial.hs
@@ -19,21 +19,39 @@ import Math.Polynomial.Pretty ({- instance -})
 import Data.List
 import Data.List.ZipSum
 
+-- |The polynomial \"0\"
 zero :: Num a => Poly a
 zero = poly LE []
 
+-- |The polynomial \"1\"
 one :: Num a => Poly a
-one = poly LE [1]
+one = constPoly 1
 
+-- |The polynomial (in x) \"x\"
 x :: Num a => Poly a
 x = poly LE [0,1]
 
+-- |Given some constant 'k', construct the polynomial whose value is 
+-- constantly 'k'.
+constPoly :: Num a => a -> Poly a
+constPoly x = poly LE [x]
+
+-- |Given some scalar 's' and a polynomial 'f', computes the polynomial 'g'
+-- such that:
+-- 
+-- > evalPoly g x = s * evalPoly f x
 scalePoly :: Num a => a -> Poly a -> Poly a
 scalePoly s p = fmap (s*) p
 
+-- |Given some polynomial 'f', computes the polynomial 'g' such that:
+-- 
+-- > evalPoly g x = negate (evalPoly f x)
 negatePoly :: Num a => Poly a -> Poly a
 negatePoly = fmap negate
 
+-- |Given polynomials 'f' and 'g', computes the polynomial 'h' such that:
+-- 
+-- > evalPoly h x = evalPoly f x + evalPoly g x
 addPoly :: Num a => Poly a -> Poly a -> Poly a
 addPoly (polyCoeffs LE ->  a) (polyCoeffs LE ->  b) = poly LE (zipSum a b)
 
@@ -44,6 +62,9 @@ sumPolys :: Num a => [Poly a] -> Poly a
 sumPolys [] = zero
 sumPolys ps = poly LE (foldl1 zipSum (map (polyCoeffs LE) ps))
 
+-- |Given polynomials 'f' and 'g', computes the polynomial 'h' such that:
+-- 
+-- > evalPoly h x = evalPoly f x * evalPoly g x
 multPoly :: Num a => Poly a -> Poly a -> Poly a
 multPoly (polyCoeffs LE -> xs) (polyCoeffs LE -> ys) = poly LE $ multX ys
     where
@@ -54,14 +75,24 @@ multPoly (polyCoeffs LE -> xs) (polyCoeffs LE -> ys) = poly LE $ multX ys
             , x /= 0
             ]
 
+-- |Given a polynomial 'f' and exponent 'n', computes the polynomial 'g'
+-- such that:
+-- 
+-- > evalPoly g x = evalPoly f x ^ n
 powPoly :: (Num a, Integral b) => Poly a -> b -> Poly a
 powPoly _ 0 = poly LE [1]
 powPoly p 1 = p
 powPoly p n
+    | n < 0     = error "powPoly: negative exponent"
     | odd n     = p `multPoly` powPoly p (n-1)
     | otherwise = (\x -> multPoly x x) (powPoly p (n`div`2))
 
+-- |Given polynomials @a@ and @b@, with @b@ not 'zero', computes polynomials
+-- @q@ and @r@ such that:
+-- 
+-- > addPoly (multPoly q b) r == a
 quotRemPoly :: Fractional a => Poly a -> Poly a -> (Poly a, Poly a)
+quotRemPoly a b | polyIsZero b = error "quotRemPoly: divide by zero"
 quotRemPoly (polyCoeffs BE -> u) (polyCoeffs BE -> v)
     = go [] u (length u - length v)
     where
@@ -75,8 +106,11 @@ quotRemPoly (polyCoeffs BE -> u) (polyCoeffs BE -> v)
                 u' = tail (zipSum u (map (negate q0 *) v))
 
 quotPoly :: Fractional a => Poly a -> Poly a -> Poly a
-quotPoly u v = fst (quotRemPoly u v)
+quotPoly u v
+    | polyIsZero v  = error "quotPoly: divide by zero"
+    | otherwise     = fst (quotRemPoly u v)
 remPoly :: Fractional a => Poly a -> Poly a -> Poly a
+remPoly u v | polyIsZero v = error "remPoly: divide by zero"
 remPoly (polyCoeffs BE -> u) (polyCoeffs BE -> v)
     = go u (length u - length v)
     where
@@ -89,17 +123,32 @@ remPoly (polyCoeffs BE -> u) (polyCoeffs BE -> v)
                 q0 = head u / v0
                 u' = tail (zipSum u (map (negate q0 *) v))
 
+-- |@composePoly f g@ constructs the polynomial 'h' such that:
+-- 
+-- > evalPoly h = evalPoly f . evalPoly g
+composePoly :: Num a => Poly a -> Poly a -> Poly a
+composePoly (polyCoeffs LE -> cs) g = foldr mul zero cs
+    where
+        -- Implementation note: this is a hand-inlining of the following
+        -- (with the 'Num' instance in "Math.Polynomial.NumInstance"):
+        -- > composePoly f g = evalPoly (fmap constPoly f) g
+        mul c acc = addPoly (constPoly c) (multPoly acc g)
 
 evalPoly :: Num a => Poly a -> a -> a
 evalPoly (polyCoeffs LE -> cs) x = foldr mul 0 cs
     where
         mul c acc = c + acc * x
 
+-- |Evaluate a polynomial and its derivative (respectively) at a point.
 evalPolyDeriv :: Num a => Poly a -> a -> (a,a)
 evalPolyDeriv (polyCoeffs LE -> cs) x = foldr mul (0,0) cs
     where
         mul c (p, dp) = (p * x + c, dp * x + p)
 
+-- |Evaluate a polynomial and all of its nonzero derivatives at a point.  
+-- This is roughly equivalent to:
+-- 
+-- > evalPolyDerivs p x = map (`evalPoly` x) (takeWhile (not . polyIsZero) (iterate polyDeriv p))
 evalPolyDerivs :: Num a => Poly a -> a -> [a]
 evalPolyDerivs (polyCoeffs LE -> cs) x = trunc . zipWith (*) factorials $ foldr mul [] cs
     where
@@ -117,6 +166,9 @@ contractPoly (polyCoeffs LE -> cs) a = (poly LE q, r)
         cut remainder swap = (swap + remainder * a, remainder)
         (r,q) = mapAccumR cut 0 cs
 
+-- |@gcdPoly a b@ computes the highest order monic polynomial that is a 
+-- divisor of both @a@ and @b@.  If both @a@ and @b@ are 'zero', the 
+-- result is undefined.
 gcdPoly :: Fractional a => Poly a -> Poly a -> Poly a
 gcdPoly a b 
     | polyIsZero b  = if polyIsZero a
@@ -130,7 +182,7 @@ monic p = case polyCoeffs BE p of
     []      -> poly BE []
     (c:cs)  -> poly BE (1:map (/c) cs)
 
-
+-- |Compute the derivative of a polynomial.
 polyDeriv :: Num a => Poly a -> Poly a
 polyDeriv (polyCoeffs LE -> cs) = poly LE
     [ c * n
@@ -138,6 +190,7 @@ polyDeriv (polyCoeffs LE -> cs) = poly LE
     | n <- iterate (1+) 1
     ]
 
+-- |Compute the definite integral (from 0 to x) of a polynomial.
 polyIntegral :: Fractional a => Poly a -> Poly a
 polyIntegral (polyCoeffs LE -> cs) = poly LE $ 0 :
     [ c / n
