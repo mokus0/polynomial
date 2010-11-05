bdddd64232a53e2845eaea3c6ebc09942bb87104
James Cook <mokus@deepbondi.net>
2010-11-05 19:27:14 +0000

    Added a few more tests for the bernstein polynomials

--- test/src/Tests/Bernstein.hs
+++ test/src/Tests/Bernstein.hs
@@ -15,15 +15,44 @@ bernsteinTests =
     ]
 
 bernstein_tests =
-    [ testProperty "sum" prop_bernstein_sum
+    [ testProperty "sum"        prop_bernstein_sum
+    , testProperty "recurrence" prop_bernstein_recurrence
+    , testProperty "symmetry"   prop_bernstein_symmetry
     ]
 
+-- prop_bernstein_sum can be very time and space intensive if we let it
+-- look too far into the table.  We also don't really gain much if any 
+-- additional confidence in the correctness of the implementation by doing
+-- so, so here's a limit for 'n' in the "bernstein :: [[Poly Integer]]" tests.
+bernsteinLimit = 500
+
 prop_bernstein_sum (NonNegative n) =
-    n <= 400 ==> 
-        polyIsOne (sumPolys (bernstein !! n))
+    polyIsOne (sumPolys (bernstein !! (n `mod` bernsteinLimit)))
+
+prop_bernstein_recurrence (Positive a) (Positive b) =
+    let a' = a `mod` bernsteinLimit
+        b' = b `mod` bernsteinLimit
+        n = 1 + max a' b'; n' = fromIntegral n
+        v =     min a' b'; v' = fromIntegral v
+     in addPoly (scalePoly ((n'-v')/n') (fmap toRational (bernstein !! n !!  v   )))
+                (scalePoly ((v'+ 1)/n') (fmap toRational (bernstein !! n !! (v+1))))
+        == fmap toRational (bernstein !! (n-1) !! v)
+            
+
+prop_bernstein_symmetry (Positive a) (Positive b) =
+    let a' = a `mod` bernsteinLimit
+        b' = b `mod` bernsteinLimit
+        n = max a' b'
+        v = min a' b'
+     in bernstein !! n !! (n-v)
+     == composePoly (bernstein !! n !! v) (poly LE [1, -1])
+            
 
 evalBernstein_tests = 
-    [ testProperty "sane" prop_evalBernstein_sane
+    [ testProperty "sane"       prop_evalBernstein_sane
+    , testProperty "sign"       prop_evalBernstein_sign
+    , testProperty "local max"  prop_evalBernstein_local_max
+    , testProperty "symmetry"   prop_evalBernstein_symmetry
     ]
 
 prop_evalBernstein_sane (NonNegative a) (NonNegative b) x =
@@ -32,3 +61,39 @@ prop_evalBernstein_sane (NonNegative a) (NonNegative b) x =
      in n <= 600 ==>
             evalPoly (fmap toRational (bernstein !! n !! v)) x
             == evalBernstein n v x
+
+prop_evalBernstein_sign (NonNegative a) (NonNegative b) x =
+    let n = max a b
+        v = min a b
+     in signum (evalBernstein n v x) == evalBernstein_sign n v x
+
+prop_evalBernstein_local_max  (NonNegative a) (NonNegative b) x =
+    let n = max a b
+        v = min a b
+        xMax = fromIntegral v / fromIntegral n
+     in n >= 0 ==>
+            evalBernstein n v (onUnitInterval x) <= evalBernstein n v xMax
+
+prop_evalBernstein_symmetry  (NonNegative a) (NonNegative b) x =
+    let n = max a b
+        v = min a b
+     in evalBernstein n (n-v) x
+     == evalBernstein n    v  (1-x)
+
+-- arbitrary function mapping the real line to the unit interval
+onUnitInterval x
+    | x < 0     = onUnitInterval (negate x)
+    | x > 1     = recip x
+    | otherwise = x
+
+evalBernstein_sign 0 0 _ = 1
+evalBernstein_sign n v 0 = delta v 0
+evalBernstein_sign n v 1 = delta v n
+evalBernstein_sign n v x
+    | x > 0 && x < 1    = 1
+    | even n || x < 0   = if even v then 1 else -1
+    | otherwise         = if odd  v then 1 else -1 
+
+delta x y
+    | x == y    = 1
+    | otherwise = 0
