729ff99abe67be3a9f09c4f8c2da5d182a73e787
Adam Vogt <vogt.adam@gmail.com>
2014-06-25 20:54:07 -0400

    add Bernouli polynomial
    some credit to rwbarton for the sharing done in what is
    called biIntegralBi

---
+++ src/Math/Polynomial/Bernoulli.hs
@@ -0,0 +1,25 @@
+module Math.Polynomial.Bernoulli (bernoulliPoly) where
+
+import Math.Polynomial
+import Data.VectorSpace
+
+{- | Bernouli polynomial with a nonstandard normalization
+
+> b_i = bernouliPoly !! i
+
+Has the following generating function (C.2 in IH Sloan & S Joe
+"Lattice Methods for multiple integration" 1994 page 227)
+
+> t exp(x*t) / (exp(t) - 1) = sum_{i=0} b_i t^i
+
+The standard normalization would have @= sum_{i=0} B_i t^i / i!@
+
+-}
+bernoulliPoly :: (Fractional a, Eq a) => [Poly a]
+bernoulliPoly = map fst biIntegralBi
+
+biIntegralBi :: (Fractional a, Eq a) => [(Poly a, Poly a)]
+biIntegralBi = (constPoly 1, polyIntegral (constPoly 1)) : map f biIntegralBi
+  where f (p, ip) = case polyIntegral ip of
+                      ip2 -> case constPoly $ evalPoly ip2 0 - evalPoly ip2 1 of
+                               c -> (c `addPoly` ip, polyIntegral c `addPoly` ip2)
